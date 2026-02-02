43b03e666fb96738d7931d94417b2995e7beec69
Masitas <nereoschmidt@gmail.com>
2026-02-02 18:36:33 -0300

    Primer commit (#1)

---
+++ src/main/kotlin/com/masitasia/chatglobo/ChatGlobo.kt
@@ -0,0 +1,14 @@
+package com.masitasia.chatglobo
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChatGlobo : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
